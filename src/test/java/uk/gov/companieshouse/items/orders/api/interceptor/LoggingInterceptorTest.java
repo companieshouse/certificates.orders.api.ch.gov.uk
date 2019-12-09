53a67e62bfed027381ebba2ef8e286a36f222317
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2019-12-09 15:05:49 +0000

    PCI-438: Rename test class from ...Tests to ...Test.

--- src/test/java/uk/gov/companieshouse/items/orders/api/interceptor/LoggingInterceptorTest.java
+++ src/test/java/uk/gov/companieshouse/items/orders/api/interceptor/LoggingInterceptorTest.java
@@ -25,7 +25,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class LoggingInterceptorTests {
+class LoggingInterceptorTest {
 
     @Mock
     private HttpServletRequest httpServletRequest;
