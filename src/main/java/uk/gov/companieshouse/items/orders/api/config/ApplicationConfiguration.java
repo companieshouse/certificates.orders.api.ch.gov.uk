8ece2df746b8da506ba10de0c140e13698129599
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2019-12-16 14:26:08 +0000

    PCI-452: Configure snake case naming strategy.

--- src/main/java/uk/gov/companieshouse/items/orders/api/config/ApplicationConfiguration.java
+++ src/main/java/uk/gov/companieshouse/items/orders/api/config/ApplicationConfiguration.java
@@ -3,12 +3,15 @@ package uk.gov.companieshouse.items.orders.api.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import uk.gov.companieshouse.items.orders.api.interceptor.LoggingInterceptor;
+
+import static com.fasterxml.jackson.databind.PropertyNamingStrategy.SNAKE_CASE;
 
 @Configuration
 public class ApplicationConfiguration implements WebMvcConfigurer {
@@ -24,6 +27,7 @@ public class ApplicationConfiguration implements WebMvcConfigurer {
                 .setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL)
                 .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .setPropertyNamingStrategy(SNAKE_CASE)
                 .findAndRegisterModules();
     }
 
