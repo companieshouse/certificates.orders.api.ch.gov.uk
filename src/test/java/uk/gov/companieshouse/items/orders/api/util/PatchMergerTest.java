e24981114adafb2492dc1df35611445a6ec6d28f
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2019-12-16 17:03:22 +0000

    PCI-452: Partially unit/integration test PatchMerger.

---
+++ src/test/java/uk/gov/companieshouse/items/orders/api/util/PatchMergerTest.java
@@ -0,0 +1,157 @@
+package uk.gov.companieshouse.items.orders.api.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import uk.gov.companieshouse.items.orders.api.model.CertificateItem;
+
+import javax.json.Json;
+import javax.json.JsonMergePatch;
+import javax.json.JsonReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static com.fasterxml.jackson.databind.PropertyNamingStrategy.SNAKE_CASE;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Unit tests the {@link PatchMerger} class.
+ */
+@ExtendWith(SpringExtension.class)
+@SpringJUnitConfig(PatchMergerTest.Config.class)
+class PatchMergerTest {
+
+    @Configuration
+    static class Config {
+        @Bean
+        public ObjectMapper objectMapper() {
+            return new ObjectMapper()
+                    .setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL)
+                    .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                    .setPropertyNamingStrategy(SNAKE_CASE)
+                    .findAndRegisterModules();
+        }
+
+        @Bean
+        PatchMerger patchMerger() {
+            return new PatchMerger(objectMapper());
+        }
+
+        @Bean
+        JsonMergePatchHttpMessageConverter converter() {
+            return new JsonMergePatchHttpMessageConverter();
+        }
+    }
+
+    private static final String ORIGINAL_COMPANY_NUMBER = "1234";
+    private static final String CORRECTED_COMPANY_NUMBER = "1235";
+    private static final boolean ORIGINAL_POSTAL_DELIVERY = true;
+    private static final int ORIGINAL_QUANTITY = 20;
+
+    private static final String ORIGINAL_ADDITIONAL_INFO = "We have our reasons.";
+    private static final boolean ORIGINAL_CERT_ACC = true;
+    private static final boolean ORIGINAL_CERT_ARTS = true;
+
+    private static final String CORRECTED_ADDITIONAL_INFO = "We have our top secret reasons.";
+    private static final boolean CORRECTED_CERT_ACC = false;
+
+    @Autowired
+    private PatchMerger patchMerger;
+
+    @Test
+    @DisplayName("Unpopulated source string property does not overwrite populated target field")
+    void unpopulatedSourceStringLeavesTargetIntact() throws IOException {
+        // Given
+        final CertificateItem original = new CertificateItem();
+        original.setCompanyNumber(ORIGINAL_COMPANY_NUMBER);
+        final CertificateItem empty = new CertificateItem();
+
+        // When
+        final CertificateItem patched =
+                patchMerger.mergePatch(createMergePatchFromPojo(empty), original, CertificateItem.class);
+
+        // Then
+        assertThat(patched.getCompanyNumber(), is(ORIGINAL_COMPANY_NUMBER));
+    }
+
+    @Test
+    @DisplayName("Unpopulated source boolean property does not overwrite populated target field")
+    void unpopulatedSourceBooleanLeavesTargetIntact() throws IOException {
+        // Given
+        final CertificateItem original = new CertificateItem();
+        original.setPostalDelivery(ORIGINAL_POSTAL_DELIVERY);
+        final CertificateItem empty = new CertificateItem();
+
+        // When
+        final CertificateItem patched =
+                patchMerger.mergePatch(createMergePatchFromPojo(empty), original, CertificateItem.class);
+
+
+        // Then
+        assertThat(patched.isPostalDelivery(), is(ORIGINAL_POSTAL_DELIVERY));
+    }
+
+    @Test
+    @DisplayName("Unpopulated source integer property does not overwrite populated target field")
+    void unpopulatedIntegerPropertyDoesNotOverwrite() throws IOException {
+        // Given
+        final CertificateItem original = new CertificateItem();
+        original.setQuantity(ORIGINAL_QUANTITY);
+        final CertificateItem empty = new CertificateItem();
+
+        // When
+        final CertificateItem patched =
+                patchMerger.mergePatch(createMergePatchFromPojo(empty), original, CertificateItem.class);
+
+        // Then
+        assertThat(patched.getQuantity(), is(ORIGINAL_QUANTITY));
+    }
+
+    @Test
+    @DisplayName("Root level property is propagated correctly")
+    void sourceRootLevelPropertyPropagated() throws IOException  {
+        // Given
+        final CertificateItem original = new CertificateItem();
+        original.setCompanyNumber(ORIGINAL_COMPANY_NUMBER);
+        final CertificateItem delta = new CertificateItem();
+        delta.setCompanyNumber(CORRECTED_COMPANY_NUMBER);
+
+        // When
+        final CertificateItem patched =
+                patchMerger.mergePatch(createMergePatchFromPojo(delta), original, CertificateItem.class);
+
+        // Then
+        assertThat(patched.getCompanyNumber(), is(CORRECTED_COMPANY_NUMBER));
+    }
+
+    /**
+     * Performs the equivalent conversion to that carried out by {@link JsonMergePatchHttpMessageConverter} to
+     * facilitate integration testing of the {@link PatchMerger} class.
+     * @param pojo the POJO that represents a merge patch
+     * @return the {@link JsonMergePatch} representation
+     * @throws IOException should something unexpected happen
+     */
+    JsonMergePatch createMergePatchFromPojo(final Object pojo) throws IOException {
+        final Gson gson = new Gson();
+        final String json = gson.toJson(pojo);
+        final InputStream stream = new ByteArrayInputStream(json.getBytes());
+        final JsonReader reader = Json.createReader(stream);
+        final JsonMergePatch patch = Json.createMergePatch(reader.readValue());
+        stream.close();
+        return patch;
+    }
+
+}
