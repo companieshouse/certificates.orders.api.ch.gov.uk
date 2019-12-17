3de79481a4dd53e90493dbae94f1a337b5df5e04
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2019-12-17 14:46:35 +0000

    PCI-452: Correct integration test expected JSON.

--- src/test/java/uk/gov/companieshouse/items/orders/api/controller/CertificateItemsControllerIntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/items/orders/api/controller/CertificateItemsControllerIntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -42,6 +43,10 @@ class CertificateItemsControllerIntegrationTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    @Qualifier("patchMapper")
+    private ObjectMapper patchMapper;
 
     @Autowired
     private CertificateItemRepository repository;
@@ -88,7 +93,7 @@ class CertificateItemsControllerIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(newItem)))
                 .andExpect(status().isCreated())
-                .andExpect(content().json(objectMapper.writeValueAsString(expectedItem)))
+                .andExpect(content().json(patchMapper.writeValueAsString(expectedItem)))
                 .andExpect(jsonPath("$.item_options.cert_inc", is(true)))
                 .andExpect(jsonPath("$.item_options.cert_shar", is(true)))
                 .andExpect(jsonPath("$.item_options.cert_dissliq", is(false)))
