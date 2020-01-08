91114d5cd3e50e4d8d8f1d603f80754c0db5bc06
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2020-01-08 12:20:00 +0000

    PCI-590: Configure mapping of nested data.

--- src/test/java/uk/gov/companieshouse/items/orders/api/mapper/TestMapper.java
+++ src/test/java/uk/gov/companieshouse/items/orders/api/mapper/TestMapper.java
@@ -2,12 +2,17 @@ package uk.gov.companieshouse.items.orders.api.mapper;
 
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * Temporary class to test how to map nested data elements.
  */
 @Mapper(componentModel = "spring")
 public interface TestMapper {
+
+    @Mapping(target = "data.datum", source = "datum")
     Entity dtoToEntity(Dto dto);
+
+    @Mapping(target = "datum", source = "data.datum")
     Dto entityToDto(Entity entity);
 }
