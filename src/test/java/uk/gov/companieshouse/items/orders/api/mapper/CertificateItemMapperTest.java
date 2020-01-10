d223deb4e6d67e60834899b99f616f89df1c3f29
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2020-01-10 09:50:12 +0000

    PCI-590: Improve mapper unit tests.

--- src/test/java/uk/gov/companieshouse/items/orders/api/mapper/CertificateItemMapperTest.java
+++ src/test/java/uk/gov/companieshouse/items/orders/api/mapper/CertificateItemMapperTest.java
@@ -9,8 +9,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import uk.gov.companieshouse.items.orders.api.dto.CertificateItemDTO;
 import uk.gov.companieshouse.items.orders.api.model.CertificateItem;
-import uk.gov.companieshouse.items.orders.api.model.ItemData;
+import uk.gov.companieshouse.items.orders.api.model.CertificateItemOptions;
+import uk.gov.companieshouse.items.orders.api.model.ItemCosts;
 
+import java.util.Map;
+
+import static java.util.Collections.singletonMap;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
@@ -25,6 +29,21 @@ public class CertificateItemMapperTest {
     private static final String ID = "CHS00000000000000001";
     private static final String COMPANY_NUMBER = "00006444";
     private static final int QUANTITY = 10;
+    private static final String DESCRIPTION = "Certificate";
+    private static final String DESCRIPTION_IDENTIFIER = "Description Identifier";
+    private static final Map<String, String> DESCRIPTION_VALUES = singletonMap("key1", "value1");
+    private static final ItemCosts ITEM_COSTS = new ItemCosts("1", "2", "3", "4");
+    private static final String KIND = "certificate";
+    private static final boolean POSTAL_DELIVERY = true;
+    private static final boolean CERT_ACC = true;
+
+    private static final CertificateItemOptions ITEM_OPTIONS;
+
+    static {
+        ITEM_OPTIONS = new CertificateItemOptions();
+        ITEM_OPTIONS.setAdditionalInformation("Additional info");
+        ITEM_OPTIONS.setCertAcc(CERT_ACC);
+    }
 
     @Configuration
     @ComponentScan(basePackageClasses = CertificateItemMapperTest.class)
@@ -34,43 +53,59 @@ public class CertificateItemMapperTest {
     private CertificateItemMapper mapperUnderTest;
 
     @Test
-    void testDtoToEntityMappingMethod() {
+    void testCertificateItemDtoToEntityMapping() {
         final CertificateItemDTO dto = new CertificateItemDTO();
         dto.setId(ID);
         dto.setCompanyNumber(COMPANY_NUMBER);
         dto.setQuantity(QUANTITY);
+        dto.setDescription(DESCRIPTION);
+        dto.setDescriptionIdentifier(DESCRIPTION_IDENTIFIER);
+        dto.setDescriptionValues(DESCRIPTION_VALUES);
+        dto.setItemCosts(ITEM_COSTS);
+        dto.setKind(KIND);
+        dto.setPostalDelivery(POSTAL_DELIVERY);
+        dto.setItemOptions(ITEM_OPTIONS);
         final CertificateItem item = mapperUnderTest.certificateItemDTOtoCertificateItem(dto);
 
         assertThat(item.getId(), is(dto.getId()));
         assertThat(item.getData(), is(notNullValue()));
-        assertThat(item.getData().getCompanyNumber(), is(dto.getCompanyNumber()));
-        assertThat(item.getData().getQuantity(), is(dto.getQuantity()));
-
-        // TODO
-//        assertThat(entity.getId(), is(dto.getId()));
-//        assertThat(entity.getData(), is(notNullValue()));
-//        assertThat(entity.getData().getDatum(), is(dto.getDatum()));
+        assertThat(item.getData().getId(), is(dto.getId()));
+        assertThat(item.getCompanyNumber(), is(dto.getCompanyNumber()));
+        assertThat(item.getQuantity(), is(dto.getQuantity()));
+        assertThat(item.getDescription(), is(dto.getDescription()));
+        assertThat(item.getDescriptionIdentifier(), is(dto.getDescriptionIdentifier()));
+        assertThat(item.getDescriptionValues(), is(dto.getDescriptionValues()));
+        assertThat(item.getItemCosts(), is(dto.getItemCosts()));
+        assertThat(item.getKind(), is(dto.getKind()));
+        assertThat(item.isPostalDelivery(), is(dto.isPostalDelivery()));
+        assertThat(item.getItemOptions(), is(dto.getItemOptions()));
     }
 
     @Test
-    void testEntityToDtoMappingMethod() {
+    void testCertificateItemEntityToDtoMapping() {
         final CertificateItem item = new CertificateItem();
         item.setId(ID);
-        final ItemData data = new ItemData();
-        data.setCompanyNumber(COMPANY_NUMBER);
-        data.setQuantity(QUANTITY);
-        item.setData(data);
+        item.setCompanyNumber(COMPANY_NUMBER);
+        item.setQuantity(QUANTITY);
+        item.setDescription(DESCRIPTION);
+        item.setDescriptionIdentifier(DESCRIPTION_IDENTIFIER);
+        item.setDescriptionValues(DESCRIPTION_VALUES);
+        item.setItemCosts(ITEM_COSTS);
+        item.setKind(KIND);
+        item.setPostalDelivery(POSTAL_DELIVERY);
+        item.setItemOptions(ITEM_OPTIONS);
         final CertificateItemDTO dto = mapperUnderTest.certificateItemToCertificateItemDTO(item);
 
         assertThat(dto.getId(), is(item.getId()));
-        assertThat(dto.getCompanyNumber(), is(item.getData().getCompanyNumber()));
-        assertThat(dto.getQuantity(), is(item.getData().getQuantity()));
-
-        // TODO
-//        assertThat(dto.getId(), is(entity.getId()));
-//        assertThat(dto.getDatum(), is(entity.getData().getDatum()));
-
-
+        assertThat(dto.getCompanyNumber(), is(item.getCompanyNumber()));
+        assertThat(dto.getQuantity(), is(item.getQuantity()));
+        assertThat(dto.getDescription(), is(item.getDescription()));
+        assertThat(dto.getDescriptionIdentifier(), is(item.getDescriptionIdentifier()));
+        assertThat(dto.getDescriptionValues(), is(item.getDescriptionValues()));
+        assertThat(dto.getItemCosts(), is(item.getItemCosts()));
+        assertThat(dto.getKind(), is(item.getKind()));
+        assertThat(dto.isPostalDelivery(), is(item.isPostalDelivery()));
+        assertThat(dto.getItemOptions(), is(item.getItemOptions()));
     }
 
 }
