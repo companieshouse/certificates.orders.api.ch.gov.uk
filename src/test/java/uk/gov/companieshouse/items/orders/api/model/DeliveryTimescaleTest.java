529917f9ed2b8d6829821fd0e7173272d63d5060
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2020-01-17 11:57:33 +0000

    PCI-506: Test delivery timescale name mappings in JSON.

---
+++ src/test/java/uk/gov/companieshouse/items/orders/api/model/DeliveryTimescaleTest.java
@@ -0,0 +1,25 @@
+package uk.gov.companieshouse.items.orders.api.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Unit/integration tests the {@link DeliveryTimescale} enum.
+ */
+class DeliveryTimescaleTest {
+
+   @Test
+   void serialisesCorrectlyToJson() throws JsonProcessingException {
+       final ObjectMapper mapper = new ObjectMapper();
+       final String standardJson = mapper.writeValueAsString(DeliveryTimescale.STANDARD);
+       final String sameDayJson = mapper.writeValueAsString(DeliveryTimescale.SAME_DAY);
+
+       assertThat(standardJson, is("\"standard\""));
+       assertThat(sameDayJson, is("\"same_day\""));
+   }
+
+}
