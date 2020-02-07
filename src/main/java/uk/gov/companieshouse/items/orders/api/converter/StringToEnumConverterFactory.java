685685189b939ff0723551526df459aee8e94867
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2020-02-07 13:53:23 +0000

    PCI-685: Use converter factories to reduce code duplication.

---
+++ src/main/java/uk/gov/companieshouse/items/orders/api/converter/StringToEnumConverterFactory.java
@@ -0,0 +1,25 @@
+package uk.gov.companieshouse.items.orders.api.converter;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.convert.converter.ConverterFactory;
+import org.springframework.data.convert.ReadingConverter;
+
+@ReadingConverter
+public final class StringToEnumConverterFactory implements ConverterFactory<String, Enum> {
+
+    public <T extends Enum> Converter<String, T> getConverter(Class<T> targetType) {
+        return new StringToEnumConverter(targetType);
+    }
+
+    private final class StringToEnumConverter<T extends Enum> implements Converter<String, T> {
+        private Class<T> enumType;
+
+        public StringToEnumConverter(Class<T> enumType) {
+            this.enumType = enumType;
+        }
+
+        public T convert(String source) {
+            return (T) Enum.valueOf(this.enumType, source.trim().toUpperCase());
+        }
+    }
+}
