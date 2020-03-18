12d8c2799d8b90872fd3d25a58382dc0f302ab4b
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2020-03-18 16:44:35 +0000

    GCI-805: Improve error handling.

--- src/main/java/uk/gov/companieshouse/items/orders/api/service/CompanyService.java
+++ src/main/java/uk/gov/companieshouse/items/orders/api/service/CompanyService.java
@@ -1,13 +1,21 @@
 package uk.gov.companieshouse.items.orders.api.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriTemplate;
 import uk.gov.companieshouse.api.ApiClient;
 import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.handler.exception.URIValidationException;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+
+import static uk.gov.companieshouse.items.orders.api.ItemsApiApplication.APPLICATION_NAMESPACE;
 
 @Service
 public class CompanyService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(APPLICATION_NAMESPACE);
 
     private static final UriTemplate
             GET_COMPANY_URI =
@@ -19,21 +27,30 @@ public class CompanyService {
         this.apiClientService = apiClientService;
     }
 
-    public String getCompanyName(final String companyNumber) throws Exception {
+    /**
+     * Interrogates the company profiles API to get the company name for the company number provided.
+     * @param companyNumber the number of the company
+     * @return the name for the company
+     */
+    public String getCompanyName(final String companyNumber) {
 
         final ApiClient apiClient = apiClientService.getInternalApiClient();
-        String companyName;
+        final String uri = GET_COMPANY_URI.expand(companyNumber).toString();
+        final String companyName;
 
         try {
-            final String uri = GET_COMPANY_URI.expand(companyNumber).toString();
             companyName = apiClient.company().get(uri).execute().getData().getCompanyName();
         } catch (ApiErrorResponseException ex) {
-            // TODO Is ServiceException useful here?
-            throw new /*Service*/Exception("Error retrieving Company Details", ex);
+            final String error = "Error getting company name for company number " + companyNumber;
+            LOGGER.error(error, ex);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, error, ex);
         } catch (URIValidationException ex) {
-            // TODO Is ServiceException useful here?
-            throw new /*Service*/Exception("Invalid URI for Company Details", ex);
+            // Should this happen (unlikely), it is a broken contract, hence 500.
+            final String error = "Invalid URI " + uri + " for company details";
+            LOGGER.error(error, ex);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, error, ex);
         }
+
         return companyName;
     }
 
