776c52e54c58d5dc22e388166ce76a3c2c8f06cd
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2020-09-01 15:01:52 +0100

    GCI-1316: Test and implement required environment variable checks.

--- src/main/java/uk/gov/companieshouse/certificates/orders/api/CertificatesApiApplication.java
+++ src/main/java/uk/gov/companieshouse/certificates/orders/api/CertificatesApiApplication.java
@@ -2,9 +2,19 @@ package uk.gov.companieshouse.certificates.orders.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import uk.gov.companieshouse.certificates.orders.api.environment.RequiredEnvironmentVariables;
+import uk.gov.companieshouse.environment.EnvironmentReader;
+import uk.gov.companieshouse.environment.exception.EnvironmentVariableException;
+import uk.gov.companieshouse.environment.impl.EnvironmentReaderImpl;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+
+import static uk.gov.companieshouse.certificates.orders.api.logging.LoggingConstants.APPLICATION_NAMESPACE;
 
 @SpringBootApplication
 public class CertificatesApiApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(APPLICATION_NAMESPACE);
 
 	public static void main(String[] args) {
 		if (checkEnvironmentVariables()) {
@@ -18,8 +28,19 @@ public class CertificatesApiApplication {
 	 * (<code>false</code>)
 	 */
 	static boolean checkEnvironmentVariables() {
-		// TODO GCI-1316 Implement this!
-		return false;
+		final EnvironmentReader environmentReader = new EnvironmentReaderImpl();
+		boolean allVariablesPresent = true;
+		LOGGER.info("Checking all required environment variables present");
+		for (final RequiredEnvironmentVariables variable : RequiredEnvironmentVariables.values()) {
+			try {
+				environmentReader.getMandatoryString(variable.getName());
+			} catch (EnvironmentVariableException eve) {
+				allVariablesPresent = false;
+				LOGGER.error(String.format("Required config item environment variable %s missing", variable.getName()));
+			}
+		}
+
+		return allVariablesPresent;
 	}
 
 }
