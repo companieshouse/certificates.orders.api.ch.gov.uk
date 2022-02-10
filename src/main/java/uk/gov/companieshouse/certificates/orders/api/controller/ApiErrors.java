231364fac99db754e5120c499a1586cbcc8e7d7e
phudson-ch <phudson@companieshouse.gov.uk>
2022-02-10 11:23:21 +0000

    Fix company status error
    * Use correct field name in ApiError

--- src/main/java/uk/gov/companieshouse/certificates/orders/api/controller/ApiErrors.java
+++ src/main/java/uk/gov/companieshouse/certificates/orders/api/controller/ApiErrors.java
@@ -3,24 +3,25 @@ package uk.gov.companieshouse.certificates.orders.api.controller;
 import uk.gov.companieshouse.api.error.ApiError;
 
 final class ApiErrors {
-
-    public static final String ERROR_TYPE_VALIDATION = "ch:validation";
-    public static final String STRING_LOCATION_TYPE = "string";
-
-    private static final String COMPANY_NUMBER_LOCATION = "company_number";
-    private static final String ERROR_TYPE_SERVICE = "ch:service";
     private static final String COMPANY_NOT_FOUND_ERROR = "company-not-found";
     private static final String COMPANY_NUMBER_IS_NULL_ERROR = "company-number-is-null";
     private static final String COMPANY_SERVICE_UNAVAILABLE_ERROR = "company-service-unavailable";
     private static final String COMPANY_STATUS_INVALID_ERROR = "company-status-invalid";
-
     private static final String INVALID_COMPANY_TYPE_ERROR = "invalid-company-type";
+
+    private static final String COMPANY_NUMBER_LOCATION = "company_number";
     private static final String COMPANY_TYPE_LOCATION = "company_type";
+    private static final String COMPANY_STATUS_LOCATION = "company_status";
+
+    public static final String STRING_LOCATION_TYPE = "string";
+
+    public static final String ERROR_TYPE_VALIDATION = "ch:validation";
+    private static final String ERROR_TYPE_SERVICE = "ch:service";
 
     static final ApiError ERR_COMPANY_NOT_FOUND = new ApiError(COMPANY_NOT_FOUND_ERROR, COMPANY_NUMBER_LOCATION, STRING_LOCATION_TYPE, ERROR_TYPE_VALIDATION);
     static final ApiError ERR_COMPANY_NUMBER_IS_NULL = new ApiError(COMPANY_NUMBER_IS_NULL_ERROR, COMPANY_NUMBER_LOCATION, STRING_LOCATION_TYPE, ERROR_TYPE_VALIDATION);
     static final ApiError ERR_SERVICE_UNAVAILABLE = new ApiError(COMPANY_SERVICE_UNAVAILABLE_ERROR, COMPANY_NUMBER_LOCATION, STRING_LOCATION_TYPE, ERROR_TYPE_SERVICE);
-    static final ApiError ERR_COMPANY_STATUS_INVALID = new ApiError(COMPANY_STATUS_INVALID_ERROR, COMPANY_NUMBER_LOCATION, STRING_LOCATION_TYPE, ERROR_TYPE_VALIDATION);
+    static final ApiError ERR_COMPANY_STATUS_INVALID = new ApiError(COMPANY_STATUS_INVALID_ERROR, COMPANY_STATUS_LOCATION, STRING_LOCATION_TYPE, ERROR_TYPE_VALIDATION);
     static final ApiError ERR_INVALID_COMPANY_TYPE = new ApiError(INVALID_COMPANY_TYPE_ERROR, COMPANY_TYPE_LOCATION, STRING_LOCATION_TYPE, ERROR_TYPE_VALIDATION);
 
     private ApiErrors() {}
